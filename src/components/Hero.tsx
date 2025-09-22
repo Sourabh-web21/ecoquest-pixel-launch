5090a014b5733995af9bf76b75c0b5ec4cee89c0
Ankit Mahto <ankitmahto19012005@gmail.com>
2025-09-22 19:52:46 +0530

    scroll

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -79,13 +79,6 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
-      {/* Decorative Elements */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-pixel-gold rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-pixel-gold rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
     </section>
   );
 };
